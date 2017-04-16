68d1bf5c03837a938fae3cc319baf3f89ef8dae8
sangeren <ch199108@gmail.com>
2017-04-16 17:18:03 +0800

    页面周期

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
 //app.js
 App({
   onLaunch: function () {
-    var that = this;
-    that.getUserInfo(function (userInfo) {
+    console.log('app-onLaunch')
+  },
+  onShow: function () {
+    console.log('app-onShow')
+    // var that = this;
+    // that.getUserInfo(function (userInfo) {
+    //   console.log('-------------------app-onShow-getUserInfo')
+
+    // });
+  },
+  getUserid: function (cb) {
+    var that = this
+    if (this.globalData.userid) {
+      typeof cb == "function" && cb(this.globalData.userid)
+    } else {
       //调用登录接口
       wx.login({
         success: function (res1) {
-          console.log(res1);
-          var gender = userInfo.gender - 1;
-          var url = that.host + 'UserLogin/' + res1.code + '?nickName=' + userInfo.nickName + '&avatarUrl=' + userInfo.avatarUrl + '&gender=' + userInfo.gender;
-          wx.request({
-            url: url,
-            data: {},
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+          wx.getUserInfo({
             success: function (res) {
-              that.userid = res.data;
-            },
-            fail: function (res) {
-              // fail
-            },
-            complete: function (res) {
-              // complete
-              console.log(res);
+              that.globalData.userInfo = res.userInfo
+              var gender = res.userInfo.gender - 1;
+              var url = that.host + 'UserLogin/' + res1.code + '?nickName=' + res.userInfo.nickName + '&avatarUrl=' + res.userInfo.avatarUrl + '&gender=' + gender;
+              wx.request({
+                url: url,
+                method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                success: function (res2) {
+                  that.userid = res2.data;
+                  that.globalData.userid = res2.data;
+                  typeof cb == "function" && cb(res2.data)
+                },
+                fail: function (res) {
+                  // fail
+                },
+                complete: function (res) {
+                  // complete
+                  console.log(res);
+                }
+              })
             }
           })
-        }
-      })
-    });
-  },
-  getUserInfo: function (cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      wx.getUserInfo({
-        success: function (res) {
-          that.globalData.userInfo = res.userInfo
-          typeof cb == "function" && cb(that.globalData.userInfo)
         }
       })
     }
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    userid: null
   },
   userid: '',
   host: 'https://wx.tiaomady.com/api/'
--- pages/input-purpose/input-purpose.js
+++ pages/input-purpose/input-purpose.js
@@ -8,7 +8,7 @@ Page({
     arrayAge: ['0', '1年', '2年', '3年', '4年', '5年', '6年', '7年', '8年', '9年'],
     arrayRacket: ['0', '1', '2', '3', '4', '5'],
     arrayTennis: ['0', '5', '10', '15', '20', '30', '50'],
-    userInfo: {},
+    userInfo: null,
     userid: '',
     userInformation: {
       // userBaseInfo: {},
@@ -43,65 +43,40 @@ Page({
   },
   onLoad: function () {
     //调用应用实例的方法获取全局数据-用户信息
-    var that = this;
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      console.log(userInfo);
-      that.setData({
-        userInfo: userInfo
-      })
-      //调用登录接口
-      wx.login({
-        success: function (res1) {
-          console.log(res1);
-          var gender = userInfo.gender - 1;
-          var url = app.host + 'UserLogin/' + res1.code + '?nickName=' + userInfo.nickName + '&avatarUrl=' + userInfo.avatarUrl + '&gender=' + userInfo.gender;
-          wx.request({
-            url: url,
-            data: {},
-            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            success: function (res) {
-              app.userid = res.data;
-            },
-            fail: function (res) {
-              // fail
-            },
-            complete: function (res) {
-              // complete
-              console.log(res);
-            }
-          })
-        }
-      })
-    });
-    // that.getUserInfo(function (userInfo) {
-
-    // });
   },
   onShow: function () {
-    console.log('input-purpose-onShow')
+    console.log('----------input-purpose-onShow---------2222')
     var that = this;
-    var appInstance = getApp();
-    console.log(appInstance)
-    wx.request({
-      url: appInstance.host + 'UserInformation/' + appInstance.userid,
-      data: {},
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
-      success: function (res) {
-        // success
+    console.log(app)
+    app.getUserid(function (userid) {
+      //更新数据
+      if (that.userInfo == undefined) {
         that.setData({
-          userInformation: res.data
+          userInfo: app.globalData.userInfo
         })
-      },
-      fail: function (res) {
-        // fail
-      },
-      complete: function (res) {
-        // complete
-        console.log('UserInformation')
-        console.log(res)
       }
-    });
+
+      wx.request({
+        url: app.host + 'UserInformation/' + userid,
+        data: {},
+        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        success: function (res) {
+          // success
+          that.setData({
+            userInformation: res.data
+          })
+        },
+        fail: function (res) {
+          // fail
+        },
+        complete: function (res) {
+          // complete
+          console.log('UserInformation-----------5555')
+          console.log(res)
+        }
+      });
+    })
+
+
   }
 })
